b4a61c08d21a2b8ac4434438b5fc5eb0324573a9
Enrique Cornejo <ecornejo@Enriques-Mac-Pro.local>
2016-03-03 19:13:13 +0100

    Primer prototipo

--- visualizacion.js
+++ visualizacion.js
@@ -64,9 +64,10 @@ var fondo = mapa.append("rect")
 
 
 var visualizacion = d3.select("#visualizacion")
-    .append("svg")
-    .attr("width", w * 2/3)
-    .attr("height",h);
+		      .append("svg")
+		      .attr("width", 600)
+		      .attr("height",h)
+		      .attr("id", "svgVisualizacion");
 
 var fondo2 = visualizacion
     .append("rect")
@@ -96,17 +97,314 @@ pintaDivVisualizacion = function() {
     }
 
 
-    var votosPartidos = [
+    var datosParticipacion = [
 	[
-	    "AJU", objetoComunidadActiva["AJU"]
-	],[
-	    "AVANT", objetoComunidadActiva["AVANT"]
-	],[
-	    "CCD", objetoComunidadActiva["CCD"]
+	    "Ndiputados", +objetoComunidadActiva["NDiputados"]
+	],
+	[
+	    
+	    "Vcontabili", +objetoComunidadActiva["Vcontabili"]
+	],
+	[
+	    "VAbstencio", +objetoComunidadActiva["VAbstencio"]
+	],
+	[
+	    "VNulos", +objetoComunidadActiva["VNulos"]
+	],
+	[
+	    "VBlanco", +objetoComunidadActiva["VBlanco"]
 	]
     ];
 
-    console.log(votosPartidos);
+    var votosPartidos = [
+	[
+	    "AJU", +objetoComunidadActiva["AJU"]
+	],
+	[
+	    "AVANT", +objetoComunidadActiva["AVANT"]
+	],
+	[
+	    "Coalición Canaria", +objetoComunidadActiva["CCaPNC"]
+	],
+	[
+	    "CCD", +objetoComunidadActiva["CCD"]
+	],
+	[
+	    "CENTROMODE", +objetoComunidadActiva["CENTROMODE"]
+	],
+	[
+	    "CILUS", +objetoComunidadActiva["CILUS"]
+	],
+	[
+	    "CRA", +objetoComunidadActiva["CRA"]
+	],
+	[
+	    "Cs", +objetoComunidadActiva["Cs"]
+	],
+	[
+	    "DL", +objetoComunidadActiva["DL"]
+	],
+	[
+	    "DN", +objetoComunidadActiva["DN"]
+	],
+	[
+	    "EB", +objetoComunidadActiva["EB"]
+	],
+	[
+	    "EHBildu", +objetoComunidadActiva["EHBildu"]
+	],
+	[
+	    "ELPI", +objetoComunidadActiva["ELPI"]
+	],
+	[
+	    "ENPOSITIU", +objetoComunidadActiva["ENPOSITIU"]
+	],
+	[
+	    "ERCCATSI", +objetoComunidadActiva["ERCCATSI"]
+	],
+	[
+	    "EUEX", +objetoComunidadActiva["EUEX"]
+	],
+	[
+	    "EZKERRA", +objetoComunidadActiva["EZKERRA"]
+	],
+	[
+	    "FDEE", +objetoComunidadActiva["FDEE"]
+	],
+	[
+	    "FEdelasJON", +objetoComunidadActiva["FEdelasJON"]
+	],
+	[
+	    "GBAI", +objetoComunidadActiva["GBAI"]
+	],
+	[
+	    "IFem", +objetoComunidadActiva["IFem"]
+	],
+	[
+	    "Independie", +objetoComunidadActiva["Independie"]
+	],
+	[
+	    "LIBERTATEN", +objetoComunidadActiva["LIBERTATEN"]
+	],
+	[
+	    "Ln", +objetoComunidadActiva["Ln"]
+	],
+	[
+	    "MAS", +objetoComunidadActiva["MAS"]
+	],
+	[
+	    "MES", +objetoComunidadActiva["MES"]
+	],
+	[
+	    "mlgXSI", +objetoComunidadActiva["mlgXSI"]
+	],
+	[
+	    "NOS", +objetoComunidadActiva["NOS"]
+	],
+	[
+	    "OE", +objetoComunidadActiva["OE"]
+	],
+	[
+	    "PACMA", +objetoComunidadActiva["PACMA"]
+	],
+	[
+	    "PAISVALENC", +objetoComunidadActiva["PAISVALENC"]
+	],
+	[
+	    "PARTICIPAC", +objetoComunidadActiva["PARTICIPAC"]
+	],
+	[
+	    "PCOE", +objetoComunidadActiva["PCOE"]
+	],
+	[
+	    "PCPE", +objetoComunidadActiva["PCPE"]
+	],
+	[
+	    "PFyV", +objetoComunidadActiva["PFyV"]
+	],
+	[
+	    "PH", +objetoComunidadActiva["PH"]
+	],
+	[
+	    "PLD", +objetoComunidadActiva["PLD"]
+	],
+	[
+	    "PLIB", +objetoComunidadActiva["PLIB"]
+	],
+	[
+	    "PNV", +objetoComunidadActiva["PNV"]
+	],
+	[
+	    "PODEMOS", +objetoComunidadActiva["PODEMOS"]
+	],
+	[
+	    "PP", +objetoComunidadActiva["PP"]
+	],
+	[
+	    "PREPAL", +objetoComunidadActiva["PREPAL"]
+	],
+	[
+	    "PSOE", +objetoComunidadActiva["PSOE"]
+	],
+	[
+	    "PT", +objetoComunidadActiva["PT"]
+	],
+	[
+	    "PUMMJ", +objetoComunidadActiva["PUMMJ"]
+	],
+	[
+	    "RECORTESCE", +objetoComunidadActiva["RECORTESCE"]
+	],
+	[
+	    "SAIn", +objetoComunidadActiva["SAIn"]
+	],
+	[
+	    "SOLUCIONA", +objetoComunidadActiva["SOLUCIONA"]
+	],
+	[
+	    "SOMVAL", +objetoComunidadActiva["SOMVAL"]
+	],
+	[
+	    "UDPYAR", +objetoComunidadActiva["UDPYAR"]
+	],
+	[
+	    "UPeC", +objetoComunidadActiva["UPeC"]
+	],
+	[
+	    "uniocat", +objetoComunidadActiva["uniocat"]
+	],
+	[
+	    "UPYD", +objetoComunidadActiva["UPYD"]
+	],
+	[
+	    "VOX", +objetoComunidadActiva["VOX"]
+	],
+	[
+	    "XLAIZQLV", +objetoComunidadActiva["XLAIZQLV"]
+	]
+    ];
+
+    // Solo queremos los partidos con votos en la región
+    var votosOrdenados  = votosPartidos
+	.sort(function(a, b) {
+	    return b[1] - a[1];
+	})
+	.filter(function(el) {
+	    return el[1] != 0;
+	}
+	);
+    console.log(votosOrdenados);
+
+    // Los ordenamos 
+    var votosTotales = 0;
+    for(var i = 0; i < votosOrdenados.length; i++) {
+	votosTotales = votosTotales + votosOrdenados[i][1];
+    }
+    console.log("Votos totales: " + votosTotales);
+
+    // Solo tendrán escaño los partidos con más de un 3% de votos
+    var umbral3pc = votosTotales * 0.03;
+    console.log("Umbral: " + umbral3pc);
+
+    // Borramos todo
+    d3.select("#encabezado").remove();
+    d3.select("#graficoBarras").remove();
+
+    // La visualización y el encabezado son <g>rupos de elementos
+    var encabezado = visualizacion
+	.append("g")
+	.attr("id", "encabezado")
+
+
+    // Pintamos cosméticamente en un gris muy pálido los bordes
+    // para la composición
+    encabezado.append("rect")
+	.attr("width", w * 2/3 - 50 )
+    	.attr("height", 75)
+    	.attr("x", 25)
+    	.attr("y", 25)
+	.style("fill", "white");
+
+    // Creamos los separadores
+    encabezado.append("svg:line")
+    	.attr("x1", (w * 2/3) - 225)
+    	.attr("y1", 25)
+    	.attr("x2", (w * 2/3) - 225)
+    	.attr("y2", 100)
+	.style("stroke", "black")
+	.style("stroke-width", "2px");
+
+    encabezado.append("svg:line")
+    	.attr("x1", (w * 2/3) - 125)
+    	.attr("y1", 25)
+    	.attr("x2", (w * 2/3) - 125)
+    	.attr("y2", 100)
+	.style("stroke", "black")
+	.style("stroke-width", "2px");
+
+    encabezado.append("svg:line")
+    	.attr("x1", (w * 2/3) - 25)
+    	.attr("y1", 25)
+    	.attr("x2", (w * 2/3) - 25)
+    	.attr("y2", 100)
+	.style("stroke", "black")
+	.style("stroke-width", "2px");
+
+
+    // Cargamos el nombre de la region
+    encabezado.append("text")
+	.text(regionActiva)
+    	.attr("x", 25)
+    	.attr("y", 50)
+	.attr("font-weight", "bold")
+	.attr("font-family", "sans-serif")
+        .attr("font-size", "25px");
+
+
+    console.log(datosParticipacion);
+
+    // Votos en blanco
+    encabezado.append("text")
+	.text(datosParticipacion[3][1])
+	.attr("text-anchor", "middle")
+    	.attr("x", (w * 2/3) - 75)
+    	.attr("y", 75)
+	.attr("font-weight", "bold")
+	.attr("font-family", "sans-serif")
+        .attr("font-size", "18px");
+
+   // Votos en blanco
+    encabezado.append("text")
+	.text("EN BLANCO")
+	.attr("text-anchor", "middle")
+    	.attr("x", (w * 2/3) - 75)
+    	.attr("y", 50)
+	.attr("font-weight", "bold")
+	.attr("font-family", "sans-serif")
+        .attr("font-size", "10px");
+ 
+    
+    var graficoBarras = visualizacion
+	.append("g")
+	.attr("id", "graficoBarras")
+
+    graficoBarras.append("rect")
+	.attr("width", w * 2/3 - 50 )
+    	.attr("height", h - 150)
+    	.attr("x", 25)
+    	.attr("y", 125)
+    	.style("fill", "white");
+
+    graficoBarras.selectAll("rect")
+	.data(votosPartidos)
+	.enter()
+	.append("rect")
+	.attr("class", "bar")
+	.attr("x", 0)
+    	.attr("y", 0)
+    	.attr("width", 20)
+    	.attr("height", 20)
+
 }
 
 pintaDivVisualizacion()
